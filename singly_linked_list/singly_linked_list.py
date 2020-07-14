0c3eda25d58e57fa7e2b2e06928dd8a05ee36c46
YasirHasn9 <hassanyasir223@gmail.com>
2020-07-13 19:11:11 -0700

    create add_to_tail method for adding new node

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -7,3 +7,22 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def print_list(self):
+        cur = self.head
+        while cur:
+            print(cur.data)
+            cur = cur.next
+
+    def add_to_tail(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = new_node
+
